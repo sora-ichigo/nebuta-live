bebefa93aa85f3ec5ab687e355c2eb46532103fa
igsr5 <igsr5.dev@gmail.com>
2022-09-03 20:03:43 +0900

    Showing google map

--- src/components/RootMain.tsx
+++ src/components/RootMain.tsx
@@ -1,5 +1,26 @@
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
 import React from "react";
 
+const containerStyle = {
+  width: "400vh",
+  height: "400vh",
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+};
 export const RootMain: React.FC = () => {
-  return <></>;
+  return (
+    <>
+      {process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY && (
+        <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
+          <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+            {/* Child components, such as markers, info windows, etc. */}
+            <></>
+          </GoogleMap>
+        </LoadScript>
+      )}
+    </>
+  );
 };
