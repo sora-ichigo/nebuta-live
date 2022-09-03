d77cd979d48d0a9baf43b4fe1e066b6c737df7b2
igsr5 <igsr5.dev@gmail.com>
2022-09-03 20:09:06 +0900

    Setting current location to google map

--- src/components/RootMain.tsx
+++ src/components/RootMain.tsx
@@ -1,26 +1,40 @@
 import { GoogleMap, LoadScript } from "@react-google-maps/api";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const containerStyle = {
-  width: "400vh",
-  height: "400vh",
+  width: "100vh",
+  height: "100vh",
 };
 
-const center = {
-  lat: -3.745,
-  lng: -38.523,
-};
 export const RootMain: React.FC = () => {
+  const [center, setCenter] = useState<{ lat: number; lng: number }>();
+  useEffect(() => {
+    (async () => {
+      const s = await getCurrentPosition();
+      setCenter({
+        lat: s.coords.latitude,
+        lng: s.coords.longitude,
+      });
+    })();
+  }, []);
+
   return (
     <>
       {process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY && (
         <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
-          <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+          <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={18}>
             {/* Child components, such as markers, info windows, etc. */}
-            <></>
           </GoogleMap>
         </LoadScript>
       )}
     </>
+  );
+};
+
+const getCurrentPosition = () => {
+  return new Promise(
+    (resolve: (value: GeolocationPosition) => void, reject: (reason: GeolocationPositionError) => void) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    }
   );
 };
