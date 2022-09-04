cefb331034088baeebc394ab36f8ed151361b89e
Okakakai <inoue.kai.work@gmail.com>
2022-09-04 23:54:33 +0900

    [追加] help onclick event

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,9 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
-import React from "react";
+import { useState } from "react";
 import HelpIcon from "@mui/icons-material/Help";
+import CloseIcon from "@mui/icons-material/Close";
 import { css } from "@emotion/react";
+import { IconButton } from "@mui/material";
 
 const header = () => css`
   // background-image: url("https://res.cloudinary.com/drb9hgnv3/image/upload/v1662254189/unknown_ghbp9e.png");
@@ -28,18 +30,56 @@ const text = css`
 `;
 
 const Header: React.FC = () => {
+  const [isShowHelp, setIsShowHelp] = useState(false);
   return (
-    <div css={header}>
-      <a href="/" css={text}>
-        青森ねぶた祭り
-      </a>
-      <HelpIcon
-        css={css`
-          color: #f6f6f6;
-          font-size: 24px;
-        `}
-      ></HelpIcon>
-    </div>
+    <>
+      {isShowHelp && (
+        <div
+          css={css`
+            transition: 0.5s;
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: #fff;
+            opacity: 0.9;
+            z-index: 100;
+          `}
+        >
+          <IconButton
+            aria-label="delete"
+            onClick={() => setIsShowHelp(false)}
+            css={css`
+              position: absolute;
+              top: 3%;
+              right: 6%;
+            `}
+          >
+            <CloseIcon
+              css={css`
+                font-size: 48px;
+              `}
+            ></CloseIcon>
+          </IconButton>
+        </div>
+      )}
+      <div css={header}>
+        <a href="/" css={text}>
+          青森ねぶた祭り
+        </a>
+        <HelpIcon
+          css={css`
+            color: #f6f6f6;
+            font-size: 24px;
+            cursor: pointer;
+          `}
+          onClick={(v) => {
+            setIsShowHelp(true);
+          }}
+        ></HelpIcon>
+      </div>
+    </>
   );
 };
 
