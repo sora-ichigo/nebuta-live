7ebdf671d46d1824be5ca7df71f059420e258956
igsr5 <igsr5.dev@gmail.com>
2022-09-04 15:42:07 +0900

    WIP: Moving nebuta marker

--- src/components/RootMain.tsx
+++ src/components/RootMain.tsx
@@ -1,6 +1,6 @@
 import { css } from "@emotion/react";
 import { GoogleMap, InfoWindow, LoadScript, Marker, Polyline } from "@react-google-maps/api";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import IconButton from "@mui/material/Button";
 import CloseIcon from "@mui/icons-material/Close";
 
@@ -39,6 +39,7 @@ type Nebuta = {
     lat: number;
     lng: number;
   };
+  currentRoute: { currentIndex: number; nextIndex: number };
 };
 
 export const RootMain: React.FC = () => {
@@ -46,6 +47,76 @@ export const RootMain: React.FC = () => {
   const [nebutas, setNebutas] = useState<Nebuta[]>([]);
   const [activeMarker, setActiveMarker] = useState<number | undefined>();
   const [activeNebutaDetal, setActiveNebutaDetal] = useState<number | undefined>();
+
+  useInterval({
+    onUpdate: () => {
+      const newNebuta = nebutas[0];
+      // id 1 の現在の経路を選ぶ ---------------------------------------
+      // NOTE: 曲がり角を過ぎていたら次の経路を選ぶ
+      if (paths[newNebuta.currentRoute.currentIndex].lat < paths[newNebuta.currentRoute.nextIndex].lat) {
+        if (newNebuta.location.lat >= paths[newNebuta.currentRoute.nextIndex].lat) {
+          // 次の経路をセット
+          newNebuta.currentRoute = {
+            currentIndex: newNebuta.currentRoute.nextIndex,
+            nextIndex: newNebuta.currentRoute.nextIndex + 1,
+          };
+        }
+      } else {
+        if (newNebuta.location.lat <= paths[newNebuta.currentRoute.nextIndex].lat) {
+          // 次の経路をセット
+          newNebuta.currentRoute = {
+            currentIndex: newNebuta.currentRoute.nextIndex,
+            nextIndex: newNebuta.currentRoute.nextIndex + 1,
+          };
+        }
+      }
+
+      if (paths[newNebuta.currentRoute.currentIndex].lng < paths[newNebuta.currentRoute.nextIndex].lng) {
+        if (newNebuta.location.lng >= paths[newNebuta.currentRoute.nextIndex].lng) {
+          // 次の経路をセット
+          newNebuta.currentRoute = {
+            currentIndex: newNebuta.currentRoute.nextIndex,
+            nextIndex: newNebuta.currentRoute.nextIndex + 1,
+          };
+        }
+      } else {
+        if (paths[newNebuta.currentRoute.currentIndex].lat <= paths[newNebuta.currentRoute.nextIndex].lat) {
+          // 次の経路をセット
+          newNebuta.currentRoute = {
+            currentIndex: newNebuta.currentRoute.nextIndex,
+            nextIndex: newNebuta.currentRoute.nextIndex + 1,
+          };
+        }
+      }
+
+      const currentPoint = paths[newNebuta.currentRoute.currentIndex];
+      const nextPoint = paths[newNebuta.currentRoute.nextIndex];
+      // ---------------------------------------
+
+      console.log(currentPoint, nextPoint);
+      // id 1 のねぶたの移動方向を決定 -------------
+      // (次の位置 - 現在地) / [500ms xOO]の OO の部分
+      const xValue = (nextPoint.lat - currentPoint.lat) / 30;
+      const yValue = (nextPoint.lng - currentPoint.lng) / 30;
+      // ---------------------------------------
+
+      console.log(xValue, yValue);
+      // id 1 のねぶた位置を更新 ------------------
+      const newLat = nebutas[0].location.lat + xValue;
+      const newLng = nebutas[0].location.lng + yValue;
+      newNebuta.location = { lat: newLat, lng: newLng };
+      // ---------------------------------------
+
+      // setNebutas ----------------------------
+      setNebutas([
+        newNebuta,
+        { id: 2, location: { lat: 35.6654412, lng: 139.7707679 }, currentRoute: { currentIndex: 0, nextIndex: 1 } },
+        { id: 3, location: { lat: 35.6754412, lng: 139.7707679 }, currentRoute: { currentIndex: 0, nextIndex: 1 } },
+      ]);
+      // ---------------------------------------
+    },
+    interval: 500,
+  });
 
   useEffect(() => {
     (async () => {
@@ -60,9 +131,9 @@ export const RootMain: React.FC = () => {
   useEffect(() => {
     (async () => {
       setNebutas([
-        { id: 1, location: { lat: 35.6554412, lng: 139.7607679 } },
-        { id: 2, location: { lat: 35.6654412, lng: 139.7707679 } },
-        { id: 3, location: { lat: 35.6754412, lng: 139.7707679 } },
+        { id: 1, location: { lat: 35.6554412, lng: 139.7607679 }, currentRoute: { currentIndex: 0, nextIndex: 1 } },
+        { id: 2, location: { lat: 35.6654412, lng: 139.7707679 }, currentRoute: { currentIndex: 0, nextIndex: 1 } },
+        { id: 3, location: { lat: 35.6754412, lng: 139.7707679 }, currentRoute: { currentIndex: 0, nextIndex: 1 } },
       ]);
     })();
   }, []);
@@ -182,4 +253,15 @@ const getCurrentPosition = () => {
       navigator.geolocation.getCurrentPosition(resolve, reject);
     }
   );
+};
+
+export const useInterval = ({ onUpdate, interval = 1000 }: { onUpdate: () => void; interval: number }) => {
+  const onUpdateRef = useRef<() => void>(() => {});
+  useEffect(() => {
+    onUpdateRef.current = onUpdate;
+  }, [onUpdate]);
+  useEffect(() => {
+    const timerId = setInterval(() => onUpdateRef.current(), interval);
+    return () => clearInterval(timerId);
+  }, []);
 };
